156998c372fd1a0035b962a6c59b26240e95292d
Obande Sunday <talk2obandesunday@gmail.com>
2023-09-01 09:34:17 +0100

    Updated work

---
+++ Javascript.js
@@ -0,0 +1,16 @@
+// let book = "math"
+// if (book == "history")
+// {console.log("History Book")}
+// else if (book == "math")
+// {console.log("math Book")}
+// else if (book == "economics")
+// {console.log("Economic Book")}
+
+// let book = "literature"
+// if (book == "history")
+// {console.log("History Book")}
+// else if (book == "math")
+// {console.log("math Book")}
+// else if (book == "economics")
+// {console.log("Economic Book")}
+// else{document.write("Unknown Book")}
